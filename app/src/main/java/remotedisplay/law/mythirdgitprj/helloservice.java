db2d228acffd54569068e23548fd2b8d2ebd9711
lawrencewithandroid <tobefree2233@gmail.com>
2015-09-25 17:18:10 +0800

    Service Submit

--- app/src/main/java/remotedisplay/law/mythirdgitprj/helloservice.java
+++ app/src/main/java/remotedisplay/law/mythirdgitprj/helloservice.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2015/9/25.
@@ -15,10 +16,14 @@ public class helloservice extends Service
         super();
     }
 
+    /**
+     * Hello, this is Service Start Up !!!!!
+     */
     @Override
     public void onCreate()
     {
         super.onCreate();
+        Toast.makeText(this, "I am in Service  !!!", Toast.LENGTH_SHORT).show();
     }
 
     @Nullable
