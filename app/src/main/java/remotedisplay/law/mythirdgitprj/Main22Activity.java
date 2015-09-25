e540ccb5429b9dfac8db63b94e83667e747fc5b3
lawrencewithandroid <tobefree2233@gmail.com>
2015-09-25 17:22:52 +0800

    Debug Test 102

--- app/src/main/java/remotedisplay/law/mythirdgitprj/Main22Activity.java
+++ app/src/main/java/remotedisplay/law/mythirdgitprj/Main22Activity.java
@@ -2,8 +2,8 @@ package remotedisplay.law.mythirdgitprj;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class Main22Activity extends AppCompatActivity
 {
@@ -13,30 +13,17 @@ public class Main22Activity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main22);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main22, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
+        TextView tstView = (TextView) findViewById(R.id.debugtextview);
+        tstView.setOnClickListener(new View.OnClickListener()
         {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
     }
+
+
 }
