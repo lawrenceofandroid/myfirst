6109dd9375a7202b5c717614fb72baa3aae35855
lawrencewithandroid <tobefree2233@gmail.com>
2015-09-25 17:40:12 +0800

    MasterX

--- app/src/main/java/remotedisplay/law/mythirdgitprj/dto/TAddressWithBit.java
+++ app/src/main/java/remotedisplay/law/mythirdgitprj/dto/TAddressWithBit.java
@@ -5,4 +5,16 @@ package remotedisplay.law.mythirdgitprj.dto;
  */
 public class TAddressWithBit
 {
+
+    private String address;
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
 }
